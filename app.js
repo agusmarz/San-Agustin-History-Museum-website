0e196dbf44c5e32e21b5cd605d4abdcf64f05c12
agusmarz <agusmarzioni24@gmail.com>
2025-01-28 20:28:22 -0300

    Actualizacion general

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const lightbox = document.querySelector('#contenedor-principal');
 
 const imagenActiva = document.querySelector('#img-activa');
 
-let indiceImagen = 0;   
+let indiceImagen = 0;
 
 const abreLightbox = (event) => {
     imagenActiva.src = event.target.src;
@@ -25,17 +25,17 @@ imagenes.forEach((imagen) => {
     imagen.addEventListener('click', abreLightbox)
 });
 
-btnCierra.addEventListener('click', () =>{
+btnCierra.addEventListener('click', () => {
     lightbox.style.display = 'none';
     console.log(indiceImagen);
 });
 
 const adelantaImagen = () => {
 
-    if(indiceImagen === imagenes.length-1){
+    if (indiceImagen === imagenes.length - 1) {
         indiceImagen = -1;
     }
-    imagenActiva.src = imagenes[indiceImagen+1].src;
+    imagenActiva.src = imagenes[indiceImagen + 1].src;
     indiceImagen++;
     console.log(indiceImagen);
 };
@@ -45,24 +45,24 @@ btnAdelanta.addEventListener('click', adelantaImagen);
 
 const retrocedeImagen = () => {
 
-    if(indiceImagen === 0){
+    if (indiceImagen === 0) {
         indiceImagen = imagenes.length;
     }
-    imagenActiva.src = imagenes[indiceImagen-1].src;
+    imagenActiva.src = imagenes[indiceImagen - 1].src;
     indiceImagen = indiceImagen - 1;
     console.log(indiceImagen);
 };
 
 btnRetrocede.addEventListener('click', retrocedeImagen);
 
-function iniciarMapa(){
-    var coord = {lat:-31.683804492372904, lng: -60.94211144337146 };
-    var map = new google.maps.Map(document.getElementById('mapa'),{
-      zoom: 20,
-      center: coord
+function iniciarMapa() {
+    var coord = { lat: -31.683804492372904, lng: -60.94211144337146 };
+    var map = new google.maps.Map(document.getElementById('mapa'), {
+        zoom: 17,
+        center: coord
     });
     var marker = new google.maps.Marker({
-      position: coord,
-      map: map
+        position: coord,
+        map: map
     });
 }
